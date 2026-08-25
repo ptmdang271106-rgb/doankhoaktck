edb5aaf2809188b23a675ec36cf246cd2ba722e0
ptmdang271106-rgb <ptmdang271106@gmail.com>
2026-08-25 16:14:21 +0700

    layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import FloatingContact from "@/components/FloatingContact";
 
 export const metadata: Metadata = {
   title: "Tuổi trẻ Khoa Kỹ thuật Cơ khí - CTUT",
-  description: "Cổng thông tin sinh viên & Hoạt động phong trào Đoàn Khoa Kỹ thuật Cơ khí CTUT",
+  description: "Cổng thông tin & Hoạt động phong trào Đoàn Khoa Kỹ thuật Cơ khí CTUT",
   icons: {
     icon: "/logodk.png",
     shortcut: "/logodk.png",
@@ -22,13 +12,17 @@ export const metadata: Metadata = {
   },
 };
 
-export default function RootLayout({ children }: LayoutProps<"/">) {
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="vi">
+      <body className="antialiased">
+        {children}
+        <FloatingContact />
+      </body>
     </html>
   );
 }
